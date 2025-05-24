8b04f63523960d8ab9ef1ac48dc77bbf7da36805
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-24 16:50:52 +0530

    python init

---
+++ basics/basic1.py
@@ -0,0 +1,25 @@
+print("Hello All !!")
+#single line comment
+no1 =100
+print(no1) #print("%d",no1)
+print("no1  = ",no1) #printf("no1 = %d",no1cls)
+print("type of no1 = ",type(no1)) #int -->long*
+
+pers = 78.89
+print("pers = ",pers)
+print("type of pers = ",type(pers)) #float --> double*
+
+x = 'y' #'' = ""
+print("value of x = ",x)
+print("type of x = ",type(x)) #string ==> char*
+
+
+isAvailable = True #False
+print("isAvaialable = ",isAvailable)
+print("isAvaialable = ",type(isAvailable)) #bool
+
+#int float string boolean NoneType
+
+data =None
+print("value of data = ",data)
+print("type of data = ",type(data))
