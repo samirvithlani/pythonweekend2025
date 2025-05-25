eb8ad3d46ded41939b9263a90e6b073923e38d94
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-25 12:28:06 +0530

    basic

---
+++ basics/basic2.py
@@ -0,0 +1,27 @@
+#scanf
+#input function
+#w r wa
+#w r woa
+#wo r wa
+#wo r woa
+
+#string
+# no1 = input("enter no 1:")
+# print("no1 = ",no1) #"100"
+# print("type of no 1 = ",type(no1))
+
+no1 = int(input("enter no 1:"))
+print("no1 = ",no1) #"100"
+print("type of no 1 = ",type(no1))
+
+no2 = float(input("enter no 2 :"))
+print("no2 = ",no2)
+print("type of no2 = ",type(no2))
+
+
+name = input("enter name :")
+print("name = ",name)
+
+
+isActive = bool(input("enter boolean value:"))
+print("isactive ",isActive)
