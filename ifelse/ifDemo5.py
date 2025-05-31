ddef4b214f696a344bcd34569811de8727b77b7a
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-31 17:12:41 +0530

    elif

---
+++ ifelse/ifDemo5.py
@@ -0,0 +1,10 @@
+no1 = int(input("enter number 1: "))
+no2 = int(input("enter number 2: "))
+
+#10>10
+if no1>no2:
+    print("no1 is bigger")
+elif no1 ==no2:
+    print("both no are same..")    
+else:
+    print("no2 is bigger")    
--- ifelse/ifdemo2.py
+++ ifelse/ifdemo2.py
@@ -1,12 +1,12 @@
 no1 = 100
 no2 = 20
-max=None
+x = None
 
 if no1>no2:
     print("no1 is bigger")
-    max = no1
+    x = no1
 else:
     print("no2 is bigger")    
-    max = no2
+    x = no2
 
 print("max no is = ",max)    
