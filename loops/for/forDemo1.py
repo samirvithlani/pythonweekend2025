101384eb892fe6e2512eb9f155306b289d0f4f65
samirvithlani <samir.vithlani83955@gmail.com>
2025-06-01 12:45:01 +0530

    for

---
+++ loops/for/forDemo1.py
@@ -0,0 +1,20 @@
+
+#range --> 1to 1
+#i =1
+#1 in 1 to 10 -> True
+# for i in range(1,11):
+#     print(i,end=" ")    
+
+
+#always starts with 0 and n-1
+# for i in range(11):
+#     print(i,end=" ")    
+
+
+#start from 1 ends to 20 but increment by 2
+# for i in range(1,20,2):
+#     print(i,end=" ")
+
+
+for i in range(10,0,-1):
+    print(i,end= " ")
