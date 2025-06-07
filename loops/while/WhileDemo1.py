f55da13e1e76cead5533c28e9e51e13c0b60f8ea
samirvithlani <samir.vithlani83955@gmail.com>
2025-06-07 17:27:40 +0530

    while

---
+++ loops/while/WhileDemo1.py
@@ -0,0 +1,17 @@
+'''
+    i=1
+    while(i<=10){
+        printf(i);
+        i++;
+    }
+
+'''
+i=1
+while i<=10:
+    #print(f"i = {i}")
+    i+=1 #2
+    print("i = ",i,end=" ")
+    
+
+    
+    
