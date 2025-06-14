4184ceebb7dbe0d0abb365d3bceb617764722e69
samirvithlani <samir.vithlani83955@gmail.com>
2025-06-14 17:17:47 +0530

    string

---
+++ string/stringDemo1.py
@@ -0,0 +1,27 @@
+name ="amit is goood boy"
+print(name)
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+
+l = len(name)
+print(f"len of string = {l}")
+print("len of string",l)
+
+# for i in range(0,4):
+#     print(f"{name[i]}")
+
+# for i in range(0,len(name)):
+#     print(f"{name[i]}")
+
+#for each loop
+
+# for  i in name:
+#     print(i)
+    
+#immutbale
+
+# name[0]="A"    
+# print(name)
