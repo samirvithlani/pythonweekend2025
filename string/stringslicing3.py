c819cbec47dfe1f8a29a2f77d509bf239e0d492e
samirvithlani <samir.vithlani83955@gmail.com>
2025-06-21 17:54:48 +0530

    string

---
+++ string/stringslicing3.py
@@ -0,0 +1,15 @@
+# name = input("enter name")
+
+# if name == name[::-1]:
+#     print("name is palindrome")
+# else:
+#     print("name is not palindrome...")    
+
+
+x = 1001
+
+if str(x) == str(x)[::-1]:
+    print("pallindorme.")    
+else:
+    print("not plaindrome..")    
+
