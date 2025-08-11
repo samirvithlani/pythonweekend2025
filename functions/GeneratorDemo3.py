5c6843c46fee5a4b8e49cbfe9a614d4c9208358f
samirvithlani <samir.vithlani83955@gmail.com>
2025-08-11 18:50:38 +0530

    generator

--- functions/GeneratorDemo3.py
+++ functions/GeneratorDemo3.py
@@ -9,11 +9,13 @@
     
 #1000 100    
 def getStudents(total,batchsize):
-    yield [f"student {i}" for i in range(1,11)]
-    yield [f"student {i}" for i in range(11,21)]
-    yield [f"student {i}" for i in range(21,31)]
+    #1 -->101 ,5
+    for i in range(1,total+1,batchsize):
+        #end 1 + 5 =6
+        end = i + batchsize
+        yield [f"student {s}" for  s in range(i,end) if s <= total]
 
-for i in getStudents():
+for i in getStudents(100,5):
     print(i)    
     
         
